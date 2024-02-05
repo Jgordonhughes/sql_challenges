6745599286ab751e8dd3996c56f618f245b1079a
Jgordonhughes <joshgordonhughes2003@icloud.com>
2024-02-05 17:28:33 +0000

    Update Challenge2.sql

--- Challenge2.sql
+++ Challenge2.sql
@@ -1,5 +1,6 @@
-`data-analytics-bootcamp-363212.course14.gwz_nps`
 
-Challenge 2
-
-Task: Write a SQL query to find all records where the global_note is greater than 7. This challenge introduces the concept of filtering data based on specific conditions.
+select global_note
+,case
+   when global_note >7 then ">7"
+ end as nps_challenge
+from `data-analytics-bootcamp-363212.course14.gwz_nps`
